f696889daecf2504ad4a1706a85b1d1e9b87f716
Ryan Thiele <39335530+RyanThiele@users.noreply.github.com>
2026-06-13 16:15:25 -0400

    added files needed for a module.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DynaVoice",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="DynaVoice: Extensions and enhancements for VibeVoice",
+)
